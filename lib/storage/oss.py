e51319f6a2caef7bdc41de27cdc96877fa84d392
guiyang <guiyang923@gmail.com>
2013-12-23 16:21:49 +0800

    updates

---
+++ lib/storage/oss.py
@@ -0,0 +1,39 @@
+import time
+from oss.oss_api import *
+from oss.oss_xml_handler import *
+from . import Storage
+
+logger = logging.getLogger(__name__)
+
+class OSSStorage(Storage):
+
+	def __init__(self, config):
+		pass
+
+	def makeKey(self, path):
+		return
+
+	@cache.put
+	def get_content(self, path):
+		pass
+
+	def put_content(self, path, content):
+		pass
+
+	def stream_read(self, path):
+		pass
+
+	def stream_write(self, path, fp):
+		pass
+
+	def list_directory(self, path=None):
+		pass
+
+	def exists(self, path):
+		pass
+
+	@cache.remove
+	def remove(self, path):
+		pass
+
+	def get_size(self, )
