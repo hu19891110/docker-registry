39e490296ce3c3d0a9c45b10a7b6787d4b5abc9f
guiyang <guiyang923@gmail.com>
2013-12-25 11:27:48 +0800

    updates

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ fi
 
 
 cd "$(dirname $0)"
-gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b 0.0.0.0:$REGISTRY_PORT -w $GUNICORN_WORKERS wsgi:application
+gunicorn --access-logfile - -e SETTINGS_FLAVOR prod --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b 0.0.0.0:$REGISTRY_PORT -w $GUNICORN_WORKERS wsgi:application
