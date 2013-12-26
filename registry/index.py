fef3a0e1a6300b55bc19844428ce085f51ee3209
guiyang <guiyang923@gmail.com>
2013-12-26 21:56:47 +0800

    updates

--- registry/index.py
+++ registry/index.py
@@ -57,6 +57,7 @@ def update_index_images(namespace, repository, data):
     path = store.index_images_path(namespace, repository)
     try:
         images = {}
+        print(data)
         print(path)
         print(store.get_content(path))
         data = json.loads(data) + json.loads(store.get_content(path))
