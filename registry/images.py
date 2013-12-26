81823d47eca2bcd4e55f13b52a2723e512bca2cf
guiyang <guiyang923@gmail.com>
2013-12-26 22:51:02 +0800

    updates

--- registry/images.py
+++ registry/images.py
@@ -169,9 +169,8 @@ def put_image_layer(image_id):
         print('checksum errors')
         print(csums)
         flask.session['checksum'] = csums
+        print(flask.session.get('checksum'))
         return toolkit.response()
-    print('in put_image_layer')
-    print(flask.session.get('checksum'))
     # We check if the checksums provided matches one the one we computed
     if checksum not in csums:
         logger.debug('put_image_layer: Wrong checksum')
